ba8f7c858f8df5097745d3d83758f062e7c83828
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-02-02 14:22:23 +0300

    JS-2888P-1

--- JS-2888P-1/users.js
+++ JS-2888P-1/users.js
@@ -1,37 +1,45 @@
-const fetchUsers = async () => {
+async function getData() {
+  let data;
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const userData = await response.json();
-    console.log(userData);
-    displayUsers(userData);
-  } catch (error) {
-    console.error("Kullanıcıları yüklerken bir hata oluştu", error);
-  }
-};
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/");
 
-const displayUsers = (users) => {
-  const container = document.getElementById("user-cards");
+    data = await response.json();
+    console.log(data);
+    getCards(data);
+  } catch (err) {
+    console.log("Bir hata oluştu: " + err);
+  }
+}
+
+function getCards(users) {
+  const container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
   users.forEach((user) => {
-    container.innerHTML += `
-    <div class="col-6 col-md-4 col-lg-3 mb-3 mx-4">
-    <div class="card h-100" style="width: 18rem;">
-  <div class="card-body ">
-    <h5 class="card-title">${user.name}</h5>
-    <div class="user-details">
-    <p class="card-text"><i class="me-2 fa-solid fa-user"></i>${user.username}</p>
-    <p class="card-text"><i class="me-2 fa-solid fa-location-dot"></i>${user.address.city},${user.address.street}</p>
-    <p class="card-text"><i class="me-2 fa-solid fa-building"></i>${user.company.name}</p>
-    <p class="card-text"><i class="me-2 fa-solid fa-envelope"></i>${user.email}</p>
-    <p class="card-text"><i class="me-2 fa-solid fa-phone"></i>${user.phone}</p>
-    </div>
-    <a href="#" class="mt-2 btn btn-primary">View Profile</a>
+    const col = document.createElement("div");
+    col.classList.add("col-12", "col-md-6", "col-lg-4", "col-xl-3", "g-5");
+    let card = `
+<div class="card h-100" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Temel Bilgiler <i class="fa-solid fa-user"></i> </h5>
+    <p class="card-text">id:${user.id} <br />name:${user.name}<br />username: ${user.username}</p>
+    <h5 class="card-title">Adres Bilgileri <i class="fa-solid fa-location-dot"></i></h5>
+    <p class="card-text">${user.address.street} sokak, ${user.address.suite} apartman.</p>
+    <h5 class="card-title">Şirket Bilgileri <i class="fa-solid fa-building"></i></h5> 
+    <p class="card-text">Şirket Adı:${user.company.name} <br />Slogan:${user.company.catchPhrase}</p>
+    <h5 class="card-title">İletişim Bilgileri <i class="fa-solid fa-phone"></i></h5> 
+    <p class="card-text">Email:${user.email} <br />Website:${user.website}<br />Telefon Numarası: ${user.phone}</p>
   </div>
-</div>
-</div>
-`;
+</div>`;
+    col.innerHTML = card;
+
+    row.appendChild(col);
   });
-};
+}
 
 window.onload = () => {
-  fetchUsers();
+  getData();
 };
